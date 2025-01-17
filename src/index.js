2ac1abc80f8ed691316ce2741ac533cafdea8cbe
Angel Leon <angeleonh2000@gmail.com>
2025-01-17 12:51:41 -0500

    Servidor configurado: Completed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import app from './app.js';
 
 const PORT = process.env.PUERTO || 4000;
 app.listen(PORT,()=>{
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 })
